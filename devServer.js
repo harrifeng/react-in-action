9dde175f457df5c7659860852263fc6e92444c36
Haoran Feng <haoran.feng@alo7.com>
2017-03-24 14:42:01 +0800

    express will eat up static name, so we only use /dist/main.js

--- devServer.js
+++ devServer.js
@@ -2,6 +2,9 @@ var express = require('express');
 var path = require('path');
 
 var app = express();
+
+app.use(express.static('static'));
+
 app.get('*', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
