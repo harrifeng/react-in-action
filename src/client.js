030888096bd339b32c5678e4c18a02a08fe6103d
Haoran Feng <haoran.feng@alo7.com>
2017-03-24 15:02:48 +0800

    dropzone works

--- src/client.js
+++ src/client.js
@@ -1,5 +1,22 @@
 import React from 'react';
 import { render } from 'react-dom';
 import App from './App';
+import Dropzone from 'react-dropzone';
 
-render(<App/>, document.getElementById('app'))
+var DropzoneDemo = React.createClass({
+  onDrop: function (acceptedFiles, rejectedFiles) {
+    console.log('Accepted files: ', acceptedFiles);
+    console.log('Rejected files: ', rejectedFiles);
+  },
+
+  render: function () {
+    return (
+      <div>
+        <Dropzone onDrop={this.onDrop}>
+        </Dropzone>
+      </div>
+    );
+  }
+});
+
+render(<DropzoneDemo/>, document.getElementById('app'))
